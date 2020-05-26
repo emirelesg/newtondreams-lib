8e8ae886606a6cbab6d3176f4108f1bd642751d6
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-25 20:36:31 -0500

    prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/expose.js',
   output: {
-    filename: 'newton.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
+  target: 'web',
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
   },
