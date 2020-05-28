db463b5fa284d17a529c43649ea702f7896f579b
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-27 23:01:20 -0500

    github action to build and deploy lib

--- src/expose.js
+++ src/expose.js
@@ -1,27 +1,5 @@
 import p$ from './module';
 
-// window.requestAnimationFrame = (() => {
-//   function backupTimeout(callback) {
-//     window.setTimeout(callback, 1000 / 60);
-//   }
-//   return (
-//     window.requestAnimationFrame ||
-//     window.webkitRequestAnimationFrame ||
-//     window.mozRequestAnimationFrame ||
-//     window.oRequestAnimationFrame ||
-//     window.msRequestAnimationFrame ||
-//     backupTimeout
-//   );
-// })();
-
-// window.cancelAnimationFrame = (() =>
-//   window.cancelAnimationFrame ||
-//   window.webkitCancelRequestAnimationFrame ||
-//   window.mozCancelRequestAnimationFrame ||
-//   window.oCancelRequestAnimationFrame ||
-//   window.msCancelRequestAnimationFrame ||
-//   clearTimeout)();
-
 /**
  * The libray is exposed to the window object through this object.
  * Constants can be found by accessing p$.constants.
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/expose.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/dist'),
     filename: 'app.bundle.js'
   },
   target: 'web',
